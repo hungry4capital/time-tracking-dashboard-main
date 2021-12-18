82bfc581952225bf26de987b92496fec5d00401c
hungry4capital <6098690+hungry4capital@users.noreply.github.com>
2021-12-18 12:44:53 -0700

    deploy to master

--- deploy.sh
+++ deploy.sh
@@ -19,7 +19,7 @@ git commit -m 'deploy'
 # if you are deploying to https://<USERNAME>.github.io
 # git push -f git@github.com:<USERNAME>/<USERNAME>.github.io.git main
 
-git push  -f git@github.com:hungry4capital/time-tracking-dashboard-main.git main:gh-pages
+git push  -f git@github.com:hungry4capital/time-tracking-dashboard-main.git master:gh-pages
 
 # if you are deploying to https://<USERNAME>.github.io/<REPO>
 # git push -f git@github.com:<USERNAME>/<REPO>.git main:gh-pages
